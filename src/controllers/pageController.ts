f7b4ab77b82532c6d53148d96aeddd30b884f8c3
Malvin Hariyanto <malvinhariyanto50@gmail.com>
2019-11-02 17:22:46 +0700

    Restructure user profile

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -39,8 +39,11 @@ async function myProfile(req: Request, res: Response) {
 async function userProfile(req: Request, res: Response) {
   try {
     let { id } = req.params;
-    let result: ResponseObject = await userModel.getUserById(Number(id));
-    if (!result) {
+    let userResult: ResponseObject = await userModel.getUserById(Number(id));
+    let postResult: ResponseObject = await postModel.getPostByUserId(
+      Number(id),
+    );
+    if (!userResult) {
       res.status(SERVER_OK).json({
         success: false,
         data: [],
@@ -48,12 +51,23 @@ async function userProfile(req: Request, res: Response) {
       });
       return;
     }
-
-    if (result.success) {
-      result.message = 'Successfully retrieve user profile';
-      res.status(SERVER_OK).json(generateResponse(result));
+    if (userResult.success && postResult.success) {
+      res.status(SERVER_OK).json(
+        generateResponse({
+          success: true,
+          data: [
+            {
+              user: userResult.data,
+              post: postResult.data,
+            },
+          ],
+          message: 'Successfully retrieve user profile',
+        }),
+      );
+    } else if (!userResult) {
+      res.status(SERVER_BAD_REQUEST).json(generateResponse(userResult));
     } else {
-      res.status(SERVER_BAD_REQUEST).json(generateResponse(result));
+      res.status(SERVER_BAD_REQUEST).json(generateResponse(postResult));
     }
   } catch (e) {
     res.status(SERVER_BAD_REQUEST).json(String(e));
