8342a9314bf3033a5384f2dc7b6e0a332e5b1583
danielindracahyadi <danielindrac@gmail.com>
2019-11-11 21:12:06 +0700

    [Fix] Move getChat into pageController

--- src/routes/pageRoute.ts
+++ src/routes/pageRoute.ts
@@ -10,5 +10,6 @@ pageRouter.get('/profile/:id', pageController.userProfile);
 pageRouter.get('/home', pageController.home);
 pageRouter.get('/my-request', pageController.myRequest);
 pageRouter.get('/user-request', pageController.userRequest);
+pageRouter.get('/chat', pageController.getMessage);
 
 export default pageRouter;
